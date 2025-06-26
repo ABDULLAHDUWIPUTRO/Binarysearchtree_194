00a542abc7bf5a38d5dbcde68c3012c13b9e3673
ABDULLAHDUWIPUTRO <abdullahduwiputro69@gmail.com>
2025-06-26 10:20:11 +0700

    step 6: jika value di data field untuk node baru kurang dari than that of cabang parent codingan dan membuat point dan exit

--- Binarysearchtree.cpp
+++ Binarysearchtree.cpp
@@ -56,5 +56,15 @@ class BinaryTree
             //5b exit 
             return;
         }
+
+        //step 6: if the value in the data field of the new node is less than that of parent
+        if (x < parent->info)
+        {
+            //6a: make the left chlild of parent point to the new node 
+            parent->leftchild = newNode;
+
+            //6b : exit
+            return;
+        }
     }
 };
